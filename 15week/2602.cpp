e60c86bcb3090bb1490e854372baea714553cb06
ChopangE <chosw05222@gmail.com>
2024-07-28 19:33:38 +0900

    Update 2602.cpp

--- 15week/2602.cpp
+++ 15week/2602.cpp
@@ -27,13 +27,13 @@ int main() {
 	}
 	for (int i = 2; i < n + 1; i++) {
 		for (int j = 1; j < m + 1; j++) {
-			if (t[i - 1] == demon[j - 1]) {
+			if (t[i - 1] == demon[j - 1]) {		//마법의 두루마리에 있는 문자와 같은 문자라면
 				demons[i][j] = demons[i][j - 1] + angels[i - 1][j - 1];
 			}
-			else {
+			else {								//다르다면 단순 복사.
 				demons[i][j] = demons[i][j - 1];
 			}
-			if (t[i - 1] == angel[j - 1]) {
+			if (t[i - 1] == angel[j - 1]) {		//demon의 상황과 동일
 				angels[i][j] = angels[i][j - 1] + demons[i - 1][j - 1];
 			}
 			else {
